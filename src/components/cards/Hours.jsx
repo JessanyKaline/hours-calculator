6e3752f7a8dca25e57da04946776b75c5907d344
Jessany Kaline <jessany42@gmail.com>
2022-11-22 15:26:37 -0300

    add two numbers in result

--- src/components/cards/Hours.jsx
+++ src/components/cards/Hours.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 
 
-export default function Hours() {
+export default function Hours() { 
   const [entryTime, setEntryTime] = React.useState("");
   const [departureTime, setDepartureTime] = React.useState("");
+  let   [horas, setHoras] = React.useState("");
+  
 
   function parse(horario) {
+    
     let [hora, minuto] = horario.split(":").map((v) => parseInt(v));
     if (!minuto) {
       minuto = 0;
@@ -15,17 +18,30 @@ export default function Hours() {
   }
 
   function duracao(entrada1, saida1) {
+   
     return parse(saida1) - parse(entrada1);
   }
 
+ 
+
   let jornada = duracao(entryTime, departureTime);
+ 
+  
 
   let diff = Math.abs(jornada);
+  
   if (diff !== 0) {
-    let horas = Math.floor(diff / 60);
-    let minutos = diff - horas * 60;
+    let horas = (Math.floor(diff / 60));
+    let minutos = (diff - horas * 60);
+     if(horas<10){
+      horas='0' + horas
+     };
+     if (minutos<10){
+      minutos= '0' + minutos
+     };
+  
 
-    console.log(`${horas} horas e ${minutos} minutos`);
+    //console.log(`${horas} horas e ${minutos} minutos`);
 
     return (
       <>
@@ -44,9 +60,12 @@ export default function Hours() {
               value={departureTime}
               onChange={(e) => setDepartureTime(e.target.value)}
             />
-
-            <div type="time" className="result">
-               {horas} : {minutos} 
+            
+           
+               
+            
+            <div  type="time" value= {horas} className="result" >
+               {horas}: {minutos}
             </div>
           </tr>
        </>
